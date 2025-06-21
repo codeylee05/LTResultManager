2506c1ed7873f7121d9b3e4feb7373b997243867
codeylee05 <mlefa5002@gmail.com>
2025-06-21 16:03:25 +0200

    Update urls.py

--- LTResultManager/urls.py
+++ LTResultManager/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("ltresultmanager/", include(main_urls))
+    path("", include(main_urls))
 ]
 
 if settings.DEBUG:
